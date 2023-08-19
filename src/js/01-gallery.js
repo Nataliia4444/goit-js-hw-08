363a478117adb9c6c8b8b9346f324d6c11382062
Nataliia4444 <127755653+Nataliia4444@users.noreply.github.com>
2023-08-19 18:45:08 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const img = galleryItems
+  .map(
+    img => `<li class="gallery__item">
+   <a class="gallery__link" href=${img.original}>
+      <img class="gallery__image" src=${img.preview} alt=${img.description} />
+   </a>
+</li>`
+  )
+  .join('');
+const image = img;
+gallery.innerHTML = image;
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDeloy: 250,
+});
+lightbox.on('show.simplelightbox', function (e) {
+  e.preventDefault();
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+const KEEP = 'feedback-form-state';
+let obj = {};
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e) {
+  e.preventovDefault();
+  form.reset();
+}
 
+form.addEventListener('input', onInputForm);
+function onInputForm() {
+  obj.email = form.email.value;
+  obj.message = form.message.value;
+}
+localStorage.setItem(KEEP, JSON.stringify(obj));
+
+const localStorage = localStorage.getItem(KEEP);
+console.log(localStorage);
