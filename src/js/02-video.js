61cfa0220259a75ebc112a4ff3782c0696611c9f
Nataliia4444 <127755653+Nataliia4444@users.noreply.github.com>
2023-08-21 22:40:43 +0300

    add changes

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,12 +10,14 @@ player.on(
   'timeupdate',
   throttle(function (iframe) {
     console.log('played the video!');
-    console.log(iframe);
+    console.log(iframe.seconds);
     localStorage.setItem(KEY, JSON.stringify(iframe));
   }),
-  20000
+  4000
 );
+
 const currentTime = localStorage.getItem(KEY);
-const seconds = JSON.parse(currentTime).seconds;
-console.log(seconds);
-player.setCurrentTime(seconds);
+if (currentTime) {
+  const seconds = JSON.parse(currentTime).seconds;
+  player.setCurrentTime(seconds);
+}
